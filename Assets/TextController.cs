e087530667a33b937d06fcd592e8683188f2ccd4
ZenBarrier <anthony.j.barrera@gmail.com>
2016-06-11 00:10:34 -0400

    State Machine
    Added a text based state machine to the text controller.

--- Assets/TextController.cs
+++ Assets/TextController.cs
@@ -1,21 +1,55 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System;
 
 public class TextController : MonoBehaviour {
 
     public Text text;
+    enum States {cell, mirror, sheets_0, locks_0, cell_mirror, sheets_1, lock_1, freedom};
+    private States myState;
 
 	// Use this for initialization
 	void Start () {
-        text.text = "Hello World!";
+        myState = States.cell;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.Space))
+        print(myState);
+        if (myState == States.cell)
         {
-            text.text = "Space key pressed.";
+            State_Cell();
+        } else if (myState == States.sheets_0)
+        {
+            State_Sheets_0();
         }
 	}
+
+    void State_Cell()
+    {
+        text.text = "You are in a prison cell and you wan to escape. " +
+                    "There are some dirty sheets on the bed, " +
+                    "a mirror on the wall. " +
+                    "and the door is locked from the outside. \n\n" +
+                    "Press S to view Sheets, M to view Mirror, L to view Lock.";
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            myState = States.sheets_0;
+        }
+    }
+
+    private void State_Sheets_0()
+    {
+        text.text = "These sheets really need a wash. " +
+                    "They have the same stain the first day I got in here. " +
+                    "Can't wait to leave! \n\n" +
+                    "Press R to return to roaming your cell.";
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            myState = States.cell;
+        }
+    }
 }
